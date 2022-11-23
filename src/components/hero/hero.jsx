dec4b8505a4e261bd1f6a8cd31b0e0327e0dc029
Chilex23 <onumaegbuchilex@gmail.com>
2022-11-23 20:59:44 +0100

    Implemented the hero section for the homepage

---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import foodPic from "../../assets/images/dronesnow.jpg";
+
+const Hero = () => (
+  <div className="my-4">
+    <img
+      src={foodPic}
+      className="w-[80%] h-[25rem] overflow-clip rounded-md absolute"
+    />
+    <div className="bg-gradient-to-r from-green-500 to-transparent h-[25rem] my-4 relative inset-0 pt-10 pl-10 rounded-md">
+      <div className="w-[45%] text-white">
+        <h1 className="text-3xl font-extrabold mb-7">Home Of Great Food!!!</h1>
+        <p className="text-lg">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad porro
+          quis excepturi ipsum quia rerum dolorem, ea quas maxime consequuntur
+          recusandae, voluptatem cumque perspiciatis accusantium commodi, in
+          odio! Quod, eveniet.
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default Hero;
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,32 +1,14 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectCount, increment } from "../redux/features/counter";
-import foodPic from "../assets/images/dronesnow.jpg";
+import Hero from "../components/hero/hero";
 
 const HomePage = () => {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
   return (
     <>
-      <div className="my-4">
-        <img
-          src={foodPic}
-          className="w-[80%] h-[25rem] overflow-clip rounded-md absolute"
-        />
-        <div className="bg-gradient-to-r from-green-500 to-transparent h-[25rem] my-4 relative inset-0 pt-10 pl-10 rounded-md">
-          <div className="w-[45%] text-white">
-            <h1 className="text-3xl font-extrabold mb-7">
-              Home Of Great Food!!!
-            </h1>
-            <p className="text-lg">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad porro
-              quis excepturi ipsum quia rerum dolorem, ea quas maxime
-              consequuntur recusandae, voluptatem cumque perspiciatis
-              accusantium commodi, in odio! Quod, eveniet.
-            </p>
-          </div>
-        </div>
-      </div>
+      <Hero />
     </>
   );
 };
